199d3a768766a8821b86738b83a003f7c2a8237c
fengsh <fengsh998@163.com>
2019-02-28 14:14:54 +0800

    提交代码。

--- bootstrapdemo/src/App.js
+++ bootstrapdemo/src/App.js
@@ -13,16 +13,45 @@ class App extends Component {
       <div className="App">
         {/* <Header />
         <MainContainer /> */}
-        <div className="bg-primary">
-          <div className="bg-warning">
-            <span className="itembox border "></span>
-            {/* <span className="border-top"></span>
-            <span className="border-right"></span>
-            <span className="border-bottom"></span>
-            <span className="border-left"></span> */}
+        <div className="containerbox">
+          <div className="pt-5">
+            <span className="itembox border rounded-lg">border</span>
+            <span className="itembox border-top">top</span>
+            <span className="itembox border-right">right</span>
+            <span className="itembox border-bottom">bottom</span>
+            <span className="itembox border-left">left</span>
+            {/* border-0 itemboxborder 位置互换不受影响 */}
+            <span className="border-0 itemboxborder">移除四边</span>
+            <span className="itemboxborder border-top-0">移除top</span>
+            <span className="itemboxborder border-right-0">移除right</span>
+            <span className="itemboxborder border-bottom-0">移除</span>
+            <span className="itemboxborder border-left-0">移除left</span>
+          </div>
+          <div className="text-white">
+            <code className="language-html" data-lang="html">
+              <span className="nt">&lt;span</span> <span class="na">class=</span><span class="s">"border"</span><span class="nt">&gt;&lt;/span&gt;</span><br />
+              <span className="nt">&lt;span</span> <span class="na">class=</span><span class="s">"border-top"</span><span class="nt">&gt;&lt;/span&gt;</span><br />
+              <span className="nt">&lt;span</span> <span class="na">class=</span><span class="s">"border-right"</span><span class="nt">&gt;&lt;/span&gt;</span><br />
+              <span className="nt">&lt;span</span> <span class="na">class=</span><span class="s">"border-bottom"</span><span class="nt">&gt;&lt;/span&gt;</span><br />
+              <span className="nt">&lt;span</span> <span class="na">class=</span><span class="s">"border-left"</span><span class="nt">&gt;&lt;/span&gt;</span>
+            </code>
+          </div>
+          <div className="pb-5">
+            <span className="itembox border border-primary">primary</span>
+            <span className="itembox border border-secondary">secondary</span>
+            <span className="itembox border border-success">success</span>
+            <span className="itembox border border-danger">danger</span>
+            <span className="itembox border border-warning">warning</span>
+            <span className="itembox border border-info">info</span>
+            <span className="itembox border border-light">light</span>
+            <span className="itembox border border-dark">dark</span>
+            <span className="itembox border border-white">white</span>
           </div>
         </div>
-      </div>
+        <button type="button" class="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div >
     );
   }
 }
--- bootstrapdemo/src/index.js
+++ bootstrapdemo/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
