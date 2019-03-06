87b1e70b20035734cf9bfbb8097aa4861e9d7e59
fengsh <fengsh998@163.com>
2019-03-06 15:13:51 +0800

    上传代码。

--- webpackdemo/webpack.config.js
+++ webpackdemo/webpack.config.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const path = require('path');
 const htmlplugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 let pathsToClean = [
     'dist/*.*',
@@ -13,64 +16,76 @@ let cleanOptions = {
     dry: false
 }
 
-module.exports = {
-    entry: {
-        app: path.join(__dirname, 'src', 'index.js')
-    },
-    mode: 'development',
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map', //开发阶段的
-    plugins: [
-        new CleanWebpackPlugin(pathsToClean, cleanOptions),
-        new htmlplugin({
-            title: 'Html 插件 Demo',
-            template: path.join(__dirname, 'public', 'index.html')
-        })
-    ],
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,      //当碰到js或jsx文件时,即文件中用到import 或reqire导入js时
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ["es2015", "react"]
-                }
-            },
-            exclude: [
-                path.resolve(__dirname, './node_modules')
+module.exports = (env, args) => {
+    console.log(env);
+    console.log(args);
+    return {
+        entry: {
+            app: path.join(__dirname, 'src', 'index.js')
+        },
+        mode: 'development',
+        output: {
+            filename: 'bundle.[hash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        devtool: 'inline-source-map', //开发阶段的
+        plugins: [
+            new CleanWebpackPlugin(pathsToClean, cleanOptions),
+            new htmlplugin({
+                title: 'Html 插件 Demo',
+                template: path.join(__dirname, 'public', 'index.html')
+            })
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new UglifyJSPlugin()
             ]
         },
-        {
-            test: /\.(css)$/,
-            use: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader',
+        module: {
+            rules: [{
+                test: /\.(js|jsx)$/,      //当碰到js或jsx文件时,即文件中用到import 或reqire导入js时
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        url: true,
-                        sourceMap: true,
+                        presets: ["es2015", "react"]
                     }
+                },
+                exclude: [
+                    path.resolve(__dirname, './node_modules')
+                ]
+            },
+            {
+                test: /\.(css)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
                 }
+            }
             ]
         },
-        {
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000
-            }
+        devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            overlay: true,
+            hot: true,
+            inline: true,
+            // quiet: true
+            stats: "none"
         }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        overlay: true,
-        hot: true,
-        inline: true
     }
 }
